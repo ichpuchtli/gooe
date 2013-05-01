f601eec1523535efa8ffe9b05dc2f8d7d2fe9738
Sam Macpherson <sam.mack91@gmail.com>
2013-05-02 00:02:16 +1000

    Introduced class structure since it will inevitably have to be used. The code was tested in QtCreator with QtCreator's Git Support

--- gooe.py
+++ gooe.py
@@ -4,23 +4,47 @@
 #TODO mpc model -> json
 
 import sys
+import random
 
 #from PyQt4.QtCore import *
 #from PyQt4.QtGui import *
 from PyQt4 import QtGui, QtCore
 
+class MediaCentre(QtGui.QWidget):
+
+    # This function simply calls the parent QWidget.__init__() function,
+    # then calls our setup function
+    def __init__(self):
+        super(MediaCentre, self).__init__()
+        self.setup()
+
+    # Setup all the windows/buttons etc..
+    def setup(self):
+
+        # Setup our window here these function are defined inthe QWidget Class
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Media Centre')
+
+        # Create a button
+        self.btn = QtGui.QPushButton('Button!', self)
+        self.btn.resize(self.btn.sizeHint())
+        self.btn.move(30,30)
+        self.btn.show()
+
+        # "Connect" a button press event with a function in this case buttonPress()
+        self.connect(self.btn, QtCore.SIGNAL("clicked()"), self.buttonPress)
+
+    # This function is "connected" to the clicked event signal from self.btn
+    def buttonPress(self):
+        # Move the button to random positions using the random module
+        self.btn.move(random.randint(0, 150), random.randint(0,100))
+
+# Create the main QApplication
 app = QtGui.QApplication(sys.argv)
 
-widget = QtGui.QWidget()
+# Create our widget & Show it
+window = MediaCentre()
+window.show()
 
-btn = QtGui.QPushButton('Button!',widget)
-
-btn.resize(btn.sizeHint())
-btn.move(50, 50)
-btn.show()
-
-widget.setGeometry(300, 300, 250, 150)
-widget.setWindowTitle('Window!')
-widget.show()
-	
+# Execute this app
 sys.exit(app.exec_())
