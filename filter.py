33b32548fac347095310faaf5781370288d4dbc5
Sam Macpherson <sam.mack91@gmail.com>
2013-05-27 03:29:00 +1000

    new static filter methods, fractional resample done with scikits.samplerate

--- filter.py
+++ filter.py
@@ -1,16 +1,69 @@
 #!/usr/bin/python2
 
-class Filter:
+import numpy
+import scikits.samplerate as sp
 
-  def apply(self, frames):
-    raise Error()
+class Filters:
 
+  @staticmethod
+  def Clone(data):
+    return data[:]
 
-class VolumeFilter(Filter):
+  @staticmethod
+  def CloneSlice(data, lower, upper):
+      return Filters.Clone(data[lower:upper])
 
-  def __init__(self, level_perc):
-    self.perc = level_perc
+  @staticmethod
+  def Replace(dest, src):
+      del dest[:]
 
-  def apply(self, frames):
-    for byte in frames:
-      byte *= self.perc
+      for i in range(len(src[:len(dest)])):
+          dest[i] = src[i]
+   
+      dest.extend(src[len(dest):])
+
+  @staticmethod
+  def InPlaceSlice(data, lower, upper):
+      del data[lower:upper]
+
+  @staticmethod
+  def UnderSample(data, factor):
+
+      copy = []
+      copy = Filters.Clone(data)
+
+      for i in range(len(data)/factor):
+          data[i] = copy[i*factor] 
+
+      del data[len(data)/factor:]
+
+      del copy
+
+  @staticmethod
+  def OverSample(data, factor):
+
+      copy = []
+      copy = Filters.Clone(data)
+
+      for i in range(len(data)):
+          for num in range(factor):
+              data[(i*factor)+num] = copy[i] 
+
+      del copy
+
+  @staticmethod
+  def ScaleVolume(data, factor):
+      for sample in data:
+          sample *= factor
+
+  @staticmethod
+  def FloatToInt(data):
+      for i in range(len(data)):
+          data[i] = int(data[i])
+
+  @staticmethod
+  def Resample(data, ratio):
+      arr = numpy.array(data)
+      Filters.Replace(data,sp.resample(arr, ratio, 'linear').tolist())
+      Filters.FloatToInt(data)
+
